d805d9c21e0e30b6899d20be3f875b0c6305f412
Pol Casacuberta <pol.casacuberta@est.fib.upc.edu>
2021-10-13 18:31:33 +0200

    added setters

--- Usuari.java
+++ Usuari.java
@@ -1,21 +1,32 @@
-public class Usuari{
+public class Usuari {
     private int id;
     private String nom;
     private String contrasenya;
+
     public Usuari(int id, String nom, String contrasenya) {
         this.id = id;
         this.nom = nom;
         this.contrasenya = contrasenya;
     }
+
     public String getNom() {
         return nom;
     }
-    public String getId(){
+
+    public int getId() {
         return id;
     }
+
     public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
     }
+
     public void setNomUsuari(String nom) {
+        this.nom = nom;
+    }
+
+    public void setIdUsuari(int id) {
+        this.id = id;
     }
 }
 
