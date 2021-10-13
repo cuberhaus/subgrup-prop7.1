923906d726e769c144e5d1ccd2b8f3cf8bcf79d1
Pol Casacuberta <pol.casacuberta@est.fib.upc.edu>
2021-10-13 19:08:44 +0200

    methods

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String args[]) {
         Usuari u1 = new Usuari(4,"Pol", "123");
-        System.out.println(u1.id);
+        System.out.println(u1.getNom());
     }
 }
--- Usuari.java
+++ Usuari.java
@@ -1,3 +1,4 @@
+package Dominio;
 public class Usuari {
     private int id;
     private String nom;
