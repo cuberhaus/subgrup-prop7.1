50377eface88704f60fff9f7394a511308980b5f
Maria Prat <mariaprat21@gmail.com>
2021-10-26 00:15:46 +0200

    Update main with Item and related classes correct and wrong usage examples

--- Main.java
+++ Main.java
@@ -1,8 +1,34 @@
-import Dominio.Usuari;
+import Domini.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main{
     public static void main(String[] args) {
         Usuari u1 = new Usuari(4,"Pol", "123");
         System.out.println(u1.getNom());
+
+        /* Correct usage of Item */
+
+        Map<String, TipusAtribut> tipusAtributMap = new HashMap<>();
+        tipusAtributMap.put("durada", new NumericEuclidia());
+
+        TipusItem song = new TipusItem("song", tipusAtributMap);
+
+        Map<String, ValorAtribut> valorAtributMap = new HashMap<>();
+        valorAtributMap.put("durada", new ValorNumeric(new NumericEuclidia(), 273));
+
+        Item disco2000 = new Item("1", song, valorAtributMap);
+        System.out.println(disco2000.getId());
+
+        /* Wrong usage
+
+        Map<String, ValorAtribut> wrongValorAtributMap = new HashMap<>();
+        wrongValorAtributMap.put("durada", new ValorNumeric(new NumericDiscret(), 258));
+
+        Item mondayMorning = new Item("1", song, wrongValorAtributMap);
+        System.out.println(mondayMorning.getId());
+
+        */
     }
 }
