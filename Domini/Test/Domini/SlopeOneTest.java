3c61073d662e8ed480f8a6ed76fd90da06916d0b
EdgarFIB <48213850+EdgarMM19@users.noreply.github.com>
2021-11-12 23:43:07 +0100

    Recomanadors

---
+++ Domini/Test/Domini/SlopeOneTest.java
@@ -0,0 +1,17 @@
+package Domini;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SlopeOneTest {
+    // TODO: picar test
+    @Test
+    void getTotesPrediccions() {
+
+    }
+
+    @Test
+    void getPrediccio() {
+    }
+}
