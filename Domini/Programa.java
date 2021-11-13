340cdc312a7f78b4434f7b8d37c65c14d25fcccb
Pol Casacuberta <pol.casacuberta@est.fib.upc.edu>
2021-11-13 11:06:59 +0100

    Valoració conté un usuari i un item, afegits getters, setters i javadoc

--- Domini/Programa.java
+++ Domini/Programa.java
@@ -5,4 +5,5 @@ package Domini;
  */
 
 public interface Programa {
+
 }
--- Domini/Valoracio.java
+++ Domini/Valoracio.java
@@ -1,21 +1,64 @@
 package Domini;
+
 /**
  * Representa una valoració.
+ *
  * @author pol.casacuberta
  */
 
 public class Valoracio {
     private double valor;
+    private Usuari usuari;
+    private Item item;
+
+    /**
+     * Consultora del item
+     *
+     * @return El resultat és l'item del P.I.
+     */
+    public Item getItem() {
+        return item;
+    }
+
+    /**
+     * Modificadora del paràmetre item
+     *
+     * @param item El paràmetre item pren el nou valor
+     */
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    /**
+     * Consultora del usuari
+     *
+     * @return El resultat és l'usuari del P.I.
+     */
+    public Usuari getUsuari() {
+        return usuari;
+    }
+
+    /**
+     * Modificadora del paràmetre usuari
+     *
+     * @param usuari El paràmetre usuari pren el nou valor
+     */
+    public void setUsuari(Usuari usuari) {
+        this.usuari = usuari;
+    }
 
     /**
      * Modificadora del paràmetre valor
-     * @param  valor El paràmetre valor pren el nou valor
+     *
+     * @param valor El paràmetre valor pren el nou valor
      */
     public void setValor(double valor) {
         this.valor = valor;
     }
+
     /**
      * Consultora del valor
+     *
      * @return El resultat és el valor del P.I.
      */
     public double getValor() {
