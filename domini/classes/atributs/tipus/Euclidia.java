b918282abc7fc7f6bbfd9d81b1c35307a09d30a9
mariaprat <maria.prat.colomer@gmail.com>
2021-11-16 01:31:02 +0100

    Add Euclidian distance class

--- domini/classes/atributs/tipus/Euclidia.java
+++ domini/classes/atributs/tipus/Euclidia.java
@@ -7,10 +7,10 @@ import domini.classes.atributs.valors.ValorNumeric;
  * Representa un tipus d'atribut numèric amb la distància euclidiana.
  * @author maria.prat
  */
-public class NumericEuclidia extends TipusAtribut {
+public class Euclidia extends TipusAtribut {
     @Override
     public TipusAtribut copy() {
-        return new NumericEuclidia();
+        return new Euclidia();
     }
 
     @Override
@@ -20,8 +20,11 @@ public class NumericEuclidia extends TipusAtribut {
 
     @Override
     public double obtenirDistancia(ValorAtribut<?> valor1, ValorAtribut<?> valor2) throws IllegalArgumentException {
-        if (!(valor1 instanceof ValorNumeric && valor2 instanceof ValorNumeric)) {
-            throw new IllegalArgumentException("Els dos ValorAtributs donats han de ser ValorNumerics.");
+        if (!(valor1.getClass().equals(valor2.getClass()))) {
+            throw new IllegalArgumentException("Els dos ValorAtributs donats han de ser instàncies de la mateixa classe.");
+        }
+        if (!admetValorAtribut(valor1)) {
+            throw new IllegalArgumentException("El TipusAtribut no admet el tipus dels ValorAtributs donats.");
         }
         return Math.abs(((ValorNumeric) valor1).getValor() - ((ValorNumeric) valor2).getValor());
     }
