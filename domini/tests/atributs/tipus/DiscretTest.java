99041dcefec30f8ea1df1f8ff85af05c9c318eac
mariaprat <maria.prat.colomer@gmail.com>
2021-11-16 11:28:36 +0100

    Add tests for Discret class

--- domini/tests/atributs/tipus/DiscretTest.java
+++ domini/tests/atributs/tipus/DiscretTest.java
@@ -1,6 +1,7 @@
 package domini.tests.atributs.tipus;
 
 import domini.classes.atributs.tipus.Discret;
+import domini.classes.atributs.tipus.TipusAtribut;
 import domini.classes.atributs.valors.*;
 import org.junit.jupiter.api.Test;
 
@@ -11,9 +12,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DiscretTest {
 
-    // TODO(maria): add more tests for other types and null values
-
     Discret discret = new Discret();
+
+    @Test
+    void copy_HauriaDeRetornarCopia() {
+        TipusAtribut copia = discret.copy();
+        assertNotSame(copia, discret);
+        assertEquals(copia, discret);
+    }
 
     @Test
     void obtenirDistancia_HauriaDEmetreExcepcio_Quan_ValorAtributsAdmissiblesSonDeClassesDiferents() {
@@ -25,11 +31,45 @@ class DiscretTest {
 
     @Test
     void obtenirDistancia_HauriaDEmetreExcepcio_Quan_ValorAtributsDeLaMateixaClasseNoSonAdmissibles() {
-        ValorConjuntBoolea valor1 = new ValorConjuntBoolea(new ArrayList<>(List.of(new ValorBoolea(true))));
         Exception exception = assertThrows(IllegalArgumentException.class, () ->
                 discret.obtenirDistancia(new ValorConjuntBoolea(new ArrayList<>(List.of(new ValorBoolea(true)))),
                         new ValorConjuntBoolea(new ArrayList<>(List.of(new ValorBoolea(false))))));
         assertEquals("El TipusAtribut no admet el tipus dels ValorAtributs donats.", exception.getMessage());
+    }
+
+    @Test
+    void obtenirDistancia_HauriaDeRetornarDistancia_Quan_ValorsBooleansSonCerts() {
+        ValorBoolea valor1 = new ValorBoolea(true);
+        ValorBoolea valor2 = new ValorBoolea(true);
+        assertEquals(discret.obtenirDistancia(valor1, valor2), 0.0);
+    }
+
+    @Test
+    void obtenirDistancia_HauriaDeRetornarDistancia_Quan_ValorsBooleansSonFalsos() {
+        ValorBoolea valor1 = new ValorBoolea(false);
+        ValorBoolea valor2 = new ValorBoolea(false);
+        assertEquals(discret.obtenirDistancia(valor1, valor2), 0.0);
+    }
+
+    @Test
+    void obtenirDistancia_HauriaDeRetornarDistancia_Quan_ValorsBooleansSonDiferents() {
+        ValorBoolea valor1 = new ValorBoolea(true);
+        ValorBoolea valor2 = new ValorBoolea(false);
+        assertEquals(discret.obtenirDistancia(valor1, valor2), 1.0);
+    }
+
+    @Test
+    void obtenirDistancia_HauriaDeRetornarDistancia_Quan_ValorsCategoricsSonIguals() {
+        ValorCategoric valor1 = new ValorCategoric("a");
+        ValorCategoric valor2 = new ValorCategoric("a");
+        assertEquals(discret.obtenirDistancia(valor1, valor2), 0.0);
+    }
+
+    @Test
+    void obtenirDistancia_HauriaDeRetornarDistancia_Quan_ValorsCategoricsSonDiferents() {
+        ValorCategoric valor1 = new ValorCategoric("a");
+        ValorCategoric valor2 = new ValorCategoric("b");
+        assertEquals(discret.obtenirDistancia(valor1, valor2), 1.0);
     }
 
     @Test
@@ -43,6 +83,20 @@ class DiscretTest {
     void obtenirDistancia_HauriaDeRetornarDistancia_Quan_ValorsNumericsSonDiferents() {
         ValorNumeric valor1 = new ValorNumeric(10.0);
         ValorNumeric valor2 = new ValorNumeric(-10.0);
+        assertEquals(discret.obtenirDistancia(valor1, valor2), 1.0);
+    }
+
+    @Test
+    void obtenirDistancia_HauriaDeRetornarDistancia_Quan_ValorsTextualsSonIguals() {
+        ValorTextual valor1 = new ValorTextual("foo");
+        ValorTextual valor2 = new ValorTextual("foo");
+        assertEquals(discret.obtenirDistancia(valor1, valor2), 0.0);
+    }
+
+    @Test
+    void obtenirDistancia_HauriaDeRetornarDistancia_Quan_ValorsTextualsSonDiferents() {
+        ValorTextual valor1 = new ValorTextual("foo");
+        ValorTextual valor2 = new ValorTextual("bar");
         assertEquals(discret.obtenirDistancia(valor1, valor2), 1.0);
     }
 }
