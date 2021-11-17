96ad1450fe9973ed57dc065d7311736921536151
mariaprat <maria.prat.colomer@gmail.com>
2021-11-17 17:38:36 +0100

    Refactor ConjuntUsuaris

--- domini/classes/ConjuntUsuaris.java
+++ domini/classes/ConjuntUsuaris.java
@@ -1,30 +1,13 @@
 package domini.classes;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
-// utilitzar obtenirValorsAtribut
-public class ConjuntUsuaris {
-    /** Conté el conjunt d'usuaris */
-    private Map<Usuari,Usuari> usuaris = new HashMap<Usuari,Usuari>();
-
-    public void inicialitzarUsuaris(String ubicacio) throws IOException {
-        LectorDeCSV lector = new LectorDeCSV();
-        TaulaCSV taula = lector.llegirCSV(ubicacio);
-        ArrayList<String> users = taula.obtenirValorsAtribut("userId");
-        for (String user : users) {
-            int id = Integer.parseInt(user);
-            Usuari u1 = new Usuari(id,true);
-            if (!usuaris.containsKey(u1)) {
-                usuaris.put(u1,u1);
-            }
+public class ConjuntUsuaris extends ConjuntIdentificat<Usuari> {
+    public void afegir(TaulaCSV taula) {
+        ArrayList<String> idsUsuaris = taula.obtenirValorsAtribut("userId");
+        for (String idUsuari : idsUsuaris) {
+            int id = Integer.parseInt(idUsuari);
+            this.afegir(new Usuari(id, true));
         }
-    }
-    public void esborraUsuari(Usuari usuari) {
-        usuari.setActiu(false);
-        usuaris.replace(usuari,usuari,usuari);
     }
 }
