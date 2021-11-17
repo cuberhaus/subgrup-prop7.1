2ee4c8e1939667f16e92476e4b77a1e488ebd0d9
mariaprat <maria.prat.colomer@gmail.com>
2021-11-17 23:02:39 +0100

    Add more tests to DiscretTest

--- domini/tests/atributs/tipus/DiscretTest.java
+++ domini/tests/atributs/tipus/DiscretTest.java
@@ -5,10 +5,9 @@ import domini.classes.atributs.tipus.TipusAtribut;
 import domini.classes.atributs.valors.*;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.*;
 
-class DiscretTest {
+public class DiscretTest {
 
     private final static double delta = 1e-10;
     private final static Discret discret = new Discret();
@@ -18,6 +17,22 @@ class DiscretTest {
         TipusAtribut copia = discret.copy();
         assertNotSame(copia, discret);
         assertEquals(copia, discret);
+    }
+
+    @Test
+    public void admetValorAtribut_HauriaDeRetornarCert() {
+        assertTrue(discret.admetValorAtribut(new ValorBoolea(true)));
+        assertTrue(discret.admetValorAtribut(new ValorCategoric("a")));
+        assertTrue(discret.admetValorAtribut(new ValorNumeric(0.0)));
+        assertTrue(discret.admetValorAtribut(new ValorTextual("a")));
+    }
+
+    @Test
+    public void admetValorAtribut_HauriaDeRetornarFals() {
+        assertFalse(discret.admetValorAtribut(new ValorConjuntBoolea(new boolean[]{true})));
+        assertFalse(discret.admetValorAtribut(new ValorConjuntCategoric(new String[]{"a"})));
+        assertFalse(discret.admetValorAtribut(new ValorConjuntNumeric(new double[]{0.0})));
+        assertFalse(discret.admetValorAtribut(new ValorConjuntTextual(new String[]{"a"})));
     }
 
     @Test(expected = IllegalArgumentException.class)
