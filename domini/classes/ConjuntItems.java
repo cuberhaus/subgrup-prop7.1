98cb5815303f6e792bf51206ad01868b217df770
pablo.vega.gallego <pablo.vega.gallego@est.fib.upc.edu>
2021-11-17 21:29:22 +0100

    Para que no me la robe pol :)

--- domini/classes/ConjuntItems.java
+++ domini/classes/ConjuntItems.java
@@ -3,6 +3,10 @@ package domini.classes;
 
 import java.util.ArrayList;
 
+/**
+ * @pablo.vega
+ */
+
 public class ConjuntItems extends ConjuntIdentificat<Item> {
     TipusItem tipusItem;
     ArrayList<Item> conjuntItems;
@@ -10,7 +14,12 @@ public class ConjuntItems extends ConjuntIdentificat<Item> {
     public ConjuntItems(String nomTipusItem, TaulaCSV taula) {
         tipusItem = new TipusItem(nomTipusItem, taula.obtenirLlistaAtributs(), taula.obtenirItem(1));
         for (int i = 0; i < taula.obtenirNumeroElements(); ++i) {
-            String sid = taula.obtenirValorsDeItemSeleccionat()
+            String sid = taula.obtenirValorAtributItem(i, "id");
+            try {
+                int id = Integer.parseInt(sid);
+            }
+            catch ()
+
             conjuntItems.add(new Item(new Id(),tipusItem,taula.obtenirItem(i)));
         }
     }
