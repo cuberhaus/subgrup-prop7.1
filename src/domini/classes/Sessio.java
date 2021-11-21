375724dca036be8bd1ae3848e1f7b0804e55949c
Pol Casacuberta <pol.casacuberta@est.fib.upc.edu>
2021-11-21 22:46:22 +0100

    arreglant javadoc

--- src/domini/classes/Sessio.java
+++ src/domini/classes/Sessio.java
@@ -9,14 +9,14 @@ public abstract class Sessio {
     /**
      * Canvia l'estat de la sessió a SessioNoIniciada
      * @throws IllegalStateException si l'estat es sessioNoIniciada
-     * @param programa c
+     * @param programa Instància de programa
      */
     abstract void tancarSessio(Programa programa);
 
     /**
      * Canvia l'estat de la sessió a SessioIniciada amb el paràmetre usuari.
      * @param usuari Usuari amb el qual iniciem la sessio.
-     * @param programa c
+     * @param programa Instancia de programa
      * @throws IllegalStateException si l'estat es sessioIniciada
      */
     abstract void iniciarSessio(Programa programa, Usuari usuari);
@@ -30,7 +30,7 @@ public abstract class Sessio {
     /**
      * Retorna l'usuari amb la sessio iniciada.
      * @throws IllegalStateException si l'estat es sessioNoIniciada
-     * @return c
+     * @return Usuari amb sessio iniciada
      */
     abstract public Usuari obtenirUsuariSessioIniciada();
 }
