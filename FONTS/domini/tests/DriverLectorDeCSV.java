f166c32cd9d11d543ce8e8fec2dce510679c062e
mariaprat <maria.prat.colomer@gmail.com>
2021-11-24 16:24:23 +0100

    Update

--- FONTS/domini/tests/DriverLectorDeCSV.java
+++ FONTS/domini/tests/DriverLectorDeCSV.java
@@ -6,8 +6,10 @@ import libs.consola;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Driver per la classe LectorDeCSV
@@ -17,7 +19,8 @@ public class DriverLectorDeCSV {
     public static void testLectorCSV() {
         try {
             LectorDeCSV lector = new LectorDeCSV();
-            TaulaCSV mockTaula = lector.llegirCSV(consola.llegirString("Introdueix la ubicacio del csv"));
+            TaulaCSV mockTaula = lector.llegirCSV(
+                    consola.llegirString("Introdueix la ubicacio del csv (i.e. ../EXE/dades_tests/items.csv):"));
             mockTaula.imprimir();
         } catch (IOException e1) {
             System.out.println("Ubicacio no valida");
@@ -26,69 +29,19 @@ public class DriverLectorDeCSV {
         }
     }
 
-    public static void testLectorCSVContingut() {
-        try {
-            String ubicacio = consola.llegirString("Introduir ubicacio de la taula");
-            LectorDeCSV lector = new LectorDeCSV();
-            TaulaCSV mockTaula = lector.llegirCSV(ubicacio);
-            ArrayList<ArrayList<String>> contingutTaula = mockTaula.obtenirTaula();
-
-            FileReader lectorFitxer = new FileReader(ubicacio);
-            BufferedReader fitxer = new BufferedReader(lectorFitxer);
-
-            String row;
-            ArrayList<ArrayList<String>> comparar = new ArrayList<>();
-            while ((row = fitxer.readLine()) != null) {
-                row = row + ' ';
-                String[] fila = row.split(",", 0);
-
-                ArrayList<String> afila = new ArrayList<>(Arrays.asList(fila));
-
-                String ultima = afila.get(afila.size() - 1);
-                StringBuilder pasos = new StringBuilder(ultima);
-                pasos.deleteCharAt(pasos.length()-1);
-
-                String result = pasos.toString();
-                afila.remove(afila.size() - 1);
-                afila.add(result);
-
-
-                comparar.add(new ArrayList<>(afila));
-                afila.clear();
-            }
-
-            if(comparar.equals(contingutTaula)) {
-                System.out.println("Contingut original: " + comparar);
-                System.out.println("Contingut de la taula: " + contingutTaula);
-                System.out.println("El contingut de la taula es el mateix al dessitjat");
-            }
-
-            else {
-                System.out.println("Hi ha un error a la implentacio");
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
     public static void main(String[] args) {
         System.out.println("Driver per la classe LectorDeCSV");
         String consulta = "\n0 - Sortir\n" +
-                "1 - Llegir CSV\n" +
-                "2 - Llegir i contingut taula\n";
-        String err = "Valor invàlid: Introdueix un enter entre 0 i 2";
+                "1 - Llegir CSV\n";
+        String err = "Valor invàlid: Introdueix un enter entre 0 i 1";
         while(true) {
             try {
-                int i = consola.llegirInt(consulta, err, 0, 2);
+                int i = consola.llegirInt(consulta, err, 0, 1);
                 switch (i) {
                     case 0:
                         return;
                     case 1:
                         testLectorCSV();
-                        break;
-
-                    case 2:
-                        testLectorCSVContingut();
                         break;
                 }
             } catch (Exception e) {
