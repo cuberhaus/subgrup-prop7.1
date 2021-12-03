fd577065ea8c4d59f1319b40220096842735d6fd
Pol Casacuberta <pol.casacuberta@est.fib.upc.edu>
2021-12-03 17:28:45 +0100

    Added templates

--- FONTS/domini/classes/ConjuntUsuaris.java
+++ FONTS/domini/classes/ConjuntUsuaris.java
@@ -21,7 +21,6 @@ public class ConjuntUsuaris extends ConjuntIdentificat<Usuari> {
      */
     public void afegir(TaulaCSV taula) {
         taula.eliminarEspaisInnecessaris();
-//        elements = new TreeMap<>();
         ArrayList<String> idsUsuaris = taula.obtenirValorsAtribut("userId");
         for (String idUsuari : idsUsuaris) {
             int id = Integer.parseInt(idUsuari);
