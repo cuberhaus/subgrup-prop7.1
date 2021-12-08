f5be0d2b92309abd89084deb798e37de1f994940
Pol Casacuberta <pol.casacuberta@est.fib.upc.edu>
2021-12-08 15:02:16 +0100

    afegits alguns butons, falta organitzar-los en l'espai

--- FONTS/presentacio/vistes/GestioUsuari.java
+++ FONTS/presentacio/vistes/GestioUsuari.java
@@ -1,20 +1,63 @@
 package presentacio.vistes;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GestioUsuari extends JPanel {
 
     public GestioUsuari() {
         this.inicialitzarGestioUsuari();
     }
+
     public void inicialitzarGestioUsuari() {
-        JButton button = new JButton();
-        this.add(button);
-    }
-    public void paintComponent(Graphics g) {
-        int width = getWidth();
-        int height = getHeight();
-        g.drawOval(0, 0, width, height);
+        JFrame frame = new JFrame();
+//        frame.add();
+        JButton afegirUsuari = new JButton("Afegir Usuari");
+        afegirUsuari.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: logica d'afegir Usuari
+            }
+        });
+        this.add(afegirUsuari);
+
+        JButton eliminarUsuari = new JButton("Eliminar Usuari");
+        eliminarUsuari.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: logica d'eliminar Usuari
+            }
+        });
+        this.add(eliminarUsuari);
+
+        JButton iniciarSessio = new JButton("Iniciar Sessió");
+        iniciarSessio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: logica d'iniciar sessio
+            }
+        });
+        this.add(iniciarSessio);
+
+        JButton tancarSessio = new JButton("Tancar Sessió");
+        tancarSessio.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: logica de tancar sessio
+            }
+        });
+        this.add(tancarSessio);
+
+        JLabel idLabel = new JLabel("Id: ");
+        this.add(idLabel);
+        JTextField idText = new JTextField();
+        idText.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Al pitjar enter fa una acció
+            }
+        });
+        this.add(idText);
     }
 }
