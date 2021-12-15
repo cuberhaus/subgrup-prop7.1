5c970c0297429257bfc62883ed2df1786f0a4257
edgarFIB <48213850+EdgarMM19@users.noreply.github.com>
2021-12-15 09:49:37 +0100

    Controlador de persistencia esquelet

---
+++ FONTS/persistencia/controladors/ControladorPersistencia.java
@@ -0,0 +1,36 @@
+package persistencia.controladors;
+
+import java.util.ArrayList;
+
+public class ControladorPersistencia {
+    public ControladorPersistencia() {}
+
+    public ArrayList<String> obtenirNomsConjunts() {
+        return null;
+    }
+    public void guardarConjuntItems(ArrayList<ArrayList<String>> conjunt, String nom) {
+    }
+    public ArrayList<String> obtenirConjuntItems(String nom) {
+        return null;
+    }
+    public void borrarConjuntItems(String nom) {
+    }
+
+
+    public void guardarConjuntUsuaris(ArrayList<ArrayList<String>> conjunt, String nom) {
+    }
+    public ArrayList<String> obtenirConjuntUsuaris(String nom) {
+        return null;
+    }
+    public void borrarConjuntUsuaris(String nom) {
+    }
+
+    public void guardarConjuntValoracions(ArrayList<ArrayList<String>> conjunt, String nom) {
+    }
+    public ArrayList<String> obtenirConjuntValoracions(String nom) {
+        return null;
+    }
+    public void borrarConjuntValoracions(String nom) {
+    }
+
+}
