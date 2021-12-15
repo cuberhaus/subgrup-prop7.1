35c5cb04ae99c2c2cfdfbb124c0d035446f1f615
edgarFIB <48213850+EdgarMM19@users.noreply.github.com>
2021-12-15 11:23:59 +0100

    Controlador persistencia fet(?)

--- FONTS/persistencia/controladors/ControladorPersistencia.java
+++ FONTS/persistencia/controladors/ControladorPersistencia.java
@@ -1,13 +1,28 @@
 package persistencia.controladors;
 
-import domini.controladors.ControladorDomini;
+import persistencia.classes.EscriptorDeCSV;
+import persistencia.classes.LectorDeCSV;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class ControladorPersistencia {
 
+    private final Path direccioCarpetaItems = Paths.get("..", "EXE", "dades");
+    private final Path direccioCarpetaUsuaris = Paths.get("..", "EXE", "usuaris");
+    private final Path direccioCarpetaValoracions = Paths.get("..", "EXE", "valoracions");
     private static ControladorPersistencia instancia;
-    private ControladorPersistencia() {}
+    private final EscriptorDeCSV escriptor;
+    private final LectorDeCSV lector;
+    private ControladorPersistencia() {
+        escriptor = new EscriptorDeCSV();
+        lector = new LectorDeCSV();
+    }
+
     public static ControladorPersistencia obtenirInstancia() {
         if (instancia == null) {
             instancia = new ControladorPersistencia();
@@ -15,32 +30,57 @@ public class ControladorPersistencia {
         return instancia;
     }
     public ArrayList<String> obtenirNomsConjunts() {
-        return null;
+        File carpetaItems = direccioCarpetaItems.toFile();
+        File[] fitxersItems = carpetaItems.listFiles();
+        ArrayList<String> noms = new ArrayList<>();
+        for (File fitxersItem : fitxersItems) {
+            String[] nomPartit = fitxersItem.getName().split("\\.");
+            noms.add(nomPartit[0]);
+            System.out.println();
+        }
+        return noms;
     }
 
-    public void guardarConjuntItems(ArrayList<ArrayList<String>> conjunt, String nom) {
+    private String obtePathConjuntItems(String nom) {
+        return direccioCarpetaItems + "/" + nom;
     }
-    public ArrayList<String> obtenirConjuntItems(String nom) {
-        return null;
+    public void guardarConjuntItems(ArrayList<ArrayList<String>> conjunt, String nom) throws IOException {
+        escriptor.escriureCSV(obtePathConjuntItems(nom), conjunt);
     }
-    public void borrarConjuntItems(String nom) {
+    public ArrayList<ArrayList<String>> obtenirConjuntItems(String nom) throws IOException {
+        return lector.llegirCSV(obtePathConjuntItems(nom));
     }
-
-
-    public void guardarConjuntUsuaris(ArrayList<ArrayList<String>> conjunt, String nom) {
-    }
-    public ArrayList<String> obtenirConjuntUsuaris(String nom) {
-        return null;
-    }
-    public void borrarConjuntUsuaris(String nom) {
+    public void borrarConjuntItems(String nom) throws IOException {
+        Path conjunt = Paths.get(obtePathConjuntItems(nom));
+        Files.delete(conjunt);
     }
 
-    public void guardarConjuntValoracions(ArrayList<ArrayList<String>> conjunt, String nom) {
+    private String obtePathConjuntUsuaris(String nom) {
+        return direccioCarpetaUsuaris + "/" + nom;
     }
-    public ArrayList<String> obtenirConjuntValoracions(String nom) {
-        return null;
+    public void guardarConjuntUsuaris(ArrayList<ArrayList<String>> conjunt, String nom) throws IOException {
+        escriptor.escriureCSV(obtePathConjuntUsuaris(nom), conjunt);
     }
-    public void borrarConjuntValoracions(String nom) {
+    public ArrayList<ArrayList<String>> obtenirConjuntUsuaris(String nom) throws IOException {
+        return lector.llegirCSV(obtePathConjuntUsuaris(nom));
+    }
+    public void borrarConjuntUsuaris(String nom) throws IOException {
+        Path conjunt = Paths.get(obtePathConjuntUsuaris(nom));
+        Files.delete(conjunt);
+    }
+
+    private String obtePathConjuntValoracions(String nom) {
+        return direccioCarpetaValoracions + "/" + nom;
+    }
+    public void guardarConjuntValoracions(ArrayList<ArrayList<String>> conjunt, String nom) throws IOException {
+        escriptor.escriureCSV(obtePathConjuntValoracions(nom), conjunt);
+    }
+    public ArrayList<ArrayList<String>> obtenirConjuntValoracions(String nom) throws IOException {
+        return lector.llegirCSV(obtePathConjuntValoracions(nom));
+    }
+    public void borrarConjuntValoracions(String nom) throws IOException {
+        Path conjunt = Paths.get(obtePathConjuntValoracions(nom));
+        Files.delete(conjunt);
     }
 
 }
