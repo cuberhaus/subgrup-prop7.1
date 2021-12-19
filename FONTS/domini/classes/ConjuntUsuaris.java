da7348016d932a09c2b305debb087846e7ce6c0f
pablo.vega.gallego <pablo.vega.gallego@est.fib.upc.edu>
2021-12-19 10:01:09 +0100

    PErdon

--- FONTS/domini/classes/ConjuntUsuaris.java
+++ FONTS/domini/classes/ConjuntUsuaris.java
@@ -21,6 +21,7 @@ public class ConjuntUsuaris extends ConjuntIdentificat<Usuari> {
     //user nom password actiu
     public ConjuntUsuaris(ArrayList<ArrayList<String>> llistaUsuaris) {
         elements = new TreeMap<>();
+        llistaUsuaris.remove(llistaUsuaris.size() - 1);
         for (ArrayList<String> usuari : llistaUsuaris) {
             String id = usuari.get(0);
             String nom = usuari.get(1);
