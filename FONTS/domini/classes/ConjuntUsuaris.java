d8316fbf91594040112767ab3aca5ada32b201a3
pablo.vega.gallego <pablo.vega.gallego@est.fib.upc.edu>
2021-12-19 09:47:01 +0100

    VA POL

--- FONTS/domini/classes/ConjuntUsuaris.java
+++ FONTS/domini/classes/ConjuntUsuaris.java
@@ -18,6 +18,22 @@ public class ConjuntUsuaris extends ConjuntIdentificat<Usuari> {
         elements = new TreeMap<>();
     }
 
+    //user nom password actiu
+    public ConjuntUsuaris(ArrayList<ArrayList<String>> llistaUsuaris) {
+        elements = new TreeMap<>();
+        for (ArrayList<String> usuari : llistaUsuaris) {
+            String id = usuari.get(0);
+            String nom = usuari.get(1);
+            String password = usuari.get(2);
+            String actiu = usuari.get(3);
+
+            Boolean actiuUsuari = Boolean.parseBoolean(actiu);
+            int idNum = Integer.parseInt(id);
+            Id idUsuari = new Id(idNum, actiuUsuari);
+            elements.put(idUsuari, new Usuari(idUsuari, nom, password));
+        }
+    }
+
     /**
      * Afegeix un conjunt de valoracions al Paràmetre implícit a partir de:
      *
