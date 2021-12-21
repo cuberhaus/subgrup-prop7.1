ae9232dffe2ee722fdd46b29929fa213dc04a4ae
Pol Casacuberta <pol.casacuberta@est.fib.upc.edu>
2021-12-21 23:52:22 +0100

    javadoc

--- FONTS/presentacio/controladors/ControladorPresentacio.java
+++ FONTS/presentacio/controladors/ControladorPresentacio.java
@@ -20,9 +20,20 @@ public class ControladorPresentacio {
     private static ControladorMenuPrincipal controladorMenuPrincipal;
     private static ControladorMenuTipusItem controladorMenuTipusItem;
 
+    /**
+     * Constructora per defecte de ControladorPresentacio
+     */
     private ControladorPresentacio() {
     }
 
+    /**
+     * Constructora de ControladorPresentacio
+     * Crea una instància única de ControladorPresentacio
+     *
+     * @return <code> ControladorPresentacio </code>
+     * @throws IOException Hi ha hagut algun error en l'entrada/sortida
+     * @throws NomInternIncorrecteException Algun valor demanat no existeix
+     */
     public static ControladorPresentacio obtenirInstancia() throws IOException, NomInternIncorrecteException {
         if (instancia == null) {
             instancia = new ControladorPresentacio();
@@ -42,46 +53,118 @@ public class ControladorPresentacio {
         return controladorDomini.obtenirNomsTipusItemsCarregats();
     }
 
+    /**
+     * Obté l'identificador de l'usuari que ha iniciat sessió
+     * @return identificador de l'usuari
+     * @throws SessioNoIniciadaException La sessió no està iniciada
+     */
     public int obtenirSessio() throws SessioNoIniciadaException {
         return controladorDomini.obtenirSessio();
     }
 
+    /**
+     * Inicia sessió amb l'identificador donat si la contrasenya és correcte
+     * @param idSessio identificador d'usuari
+     * @param contrasenya contrasenya
+     * @throws NoExisteixElementException no existeix l'identificador d'usuari
+     * @throws ContrasenyaIncorrectaException La contrasenya no és correcte
+     * @throws SessioIniciadaException La sessió ja està iniciada
+     */
     public void iniciarSessio(int idSessio, String contrasenya) throws NoExisteixElementException, ContrasenyaIncorrectaException, SessioIniciadaException {
         controladorDomini.iniciarSessio(idSessio, contrasenya);
     }
 
+    /**
+     * Retorna true si existeix l'usuari amb l'identificador donat
+     * @param id identificador d'usuari
+     * @return true si l'usuari existeix, altrament, retorna false
+     * @throws NoExisteixElementException No existeix element
+     */
     public boolean existeixUsuari(int id) throws NoExisteixElementException {
         return controladorDomini.existeixUsuari(id);
     }
 
+    /**
+     * Afegeix un usuari al conjunt d'usuaris
+     * @param nom nom de l'usuari
+     * @param contrasenya contrasenya de l'usuari
+     * @return retorna l'identificador que s'ha assignat a l'usuari
+     * @throws NoExisteixElementException No existeix element
+     * @throws JaExisteixElementException L'element ja existeix
+     */
     public int afegirUsuari(String nom, String contrasenya) throws NoExisteixElementException, JaExisteixElementException {
         return controladorDomini.afegirUsuari(nom,contrasenya);
     }
 
+    /**
+     * Esborra un usuari del conjunt
+     * @param id identificador de l'usuari
+     * @throws NoExisteixElementException L'element no existeix
+     */
     public void esborrarUsuari(int id) throws NoExisteixElementException {
         controladorDomini.esborrarUsuari(id);
     }
 
+    /**
+     * Tanca la sessió
+     * @throws SessioNoIniciadaException La sessió no està iniciada
+     */
     public void tancarSessio() throws SessioNoIniciadaException {
         controladorDomini.tancarSessio();
     }
 
+    /**
+     * Afegeix una valoració
+     * @param usuariId identificador d'usuari
+     * @param itemId identificador d'ítem
+     * @param valor valor de la valoració
+     * @throws Exception no s'ha pogut crear la valoració
+     */
     public void afegirValoracio(String usuariId, String itemId, String valor) throws Exception {
         controladorDomini.afegirValoracio(usuariId,itemId,valor);
     }
 
+    /**
+     * Consultora de si la valoració amb els identificadors donats existeix
+     * @param usuariId identificador d'usuari
+     * @param itemId identificador d'ítem
+     * @return retorna true si la valoració existeix, altrament retorna false
+     * @throws NoExisteixElementException No existeix l'element
+     */
     public boolean existeixValoracio(String usuariId, String itemId) throws NoExisteixElementException {
         return controladorDomini.existeixValoracio(usuariId,itemId);
     }
 
+    /**
+     * Esborra una valoració amb els identificadors donats
+     * @param usuariId identificador d'usuari
+     * @param itemId identificador d'ítem
+     * @throws Exception no s'ha pogut crear la valoració
+     */
     public void esborrarValoracio(String usuariId, String itemId) throws Exception {
         controladorDomini.esborraValoracio(usuariId,itemId);
     }
 
+    /**
+     * Permet canviar el valor d'una valoració donats els identificadors i el nou valor
+     * @param usuariId identificador d'usuari
+     * @param itemId identificador d'ítem
+     * @param valor valor de la valoració
+     * @throws NoExisteixElementException no existeix l'element
+     * @throws UsuariIncorrecteException l'usuari no és correcte
+     */
     public void editarValoracio(String usuariId, String itemId, String valor) throws NoExisteixElementException, UsuariIncorrecteException {
         controladorDomini.editarValoracio(usuariId,itemId,valor);
     }
 
+    /**
+     * Carrega un conjunt de valoracions a memòria des d'un arxiu extern
+     * @param pathAbsolut path on es troba l'arxiu
+     * @throws NoExisteixElementException no existeix l'element
+     * @throws IOException error en l'entrada/sortida
+     * @throws AccesAEstatIncorrecteException
+     * @throws UsuariIncorrecteException L'usuari no és correcte
+     */
     public void carregarConjuntValoracions(String pathAbsolut) throws NoExisteixElementException, IOException, AccesAEstatIncorrecteException, UsuariIncorrecteException {
         controladorDomini.carregaConjuntValoracions(pathAbsolut);
     }
@@ -98,14 +181,26 @@ public class ControladorPresentacio {
         controladorDomini.carregarTipusItem(nom, rutaAbsoluta);
     }
 
+    /**
+     * Consultora de si la sessió està iniciada o no
+     * @return retorna true si la sessió està iniciada, altrament retorna false
+     */
     public boolean esSessioIniciada() {
         return controladorDomini.esSessioIniciada();
     }
 
+    /**
+     * Exporta el conjunt d'usuaris a un arxiu extern en la localització donada
+     * @param absolutePath path de la carpeta on volem crear l'arxiu
+     * @throws IOException Error en l'entrada/sortida
+     */
     public void exportarConjuntUsuaris(String absolutePath) throws IOException {
         controladorDomini.exportarConjuntDadesUsuari(absolutePath);
     }
 
+    /**
+     * Esborra el conjunt d'usuaris
+     */
     public void esborrarConjuntUsuaris() {
         controladorDomini.esborraConjuntUsuaris();
     }
@@ -178,10 +273,17 @@ public class ControladorPresentacio {
         return controladorDomini.avaluarRecomanacio();
     }
 
+    /**
+     * Esborra totes les valoracions
+     */
     public void esborrarTotesLesValoracions() {
         controladorDomini.esborrarTotesLesValoracions();
     }
 
+    /**
+     * Obté les valoracions del tipus d'ítem seleccionat
+     * @return ArrayList amb el conjunt de valoracions
+     */
     public ArrayList<ArrayList<String>> obtenirValoracions() {
         if (!controladorDomini.existeixTipusItemSeleccionat()) {
             return new ArrayList<>();
@@ -193,22 +295,49 @@ public class ControladorPresentacio {
         controladorDomini.deseleccionarTipusItem();
     }
 
+    /**
+     * Obté el conjunt d'usuaris
+     * @return retorna un ArrayList amb el conjunt d'usuaris
+     */
     public ArrayList<ArrayList<String>> obteUsuaris() {
         return controladorDomini.obteUsuaris();
     }
 
+    /**
+     * Importa el conjunt d'usuaris des de l'arxiu en la localització donada
+     * @param absolutePath path a l'arxiu amb el conjunt d'usuaris
+     * @return ArrayList amb el conjunt d'usuaris
+     * @throws Exception no s'ha pogut importar els usuaris
+     */
     public ArrayList<String> importarUsuaris(String absolutePath) throws Exception{
         return controladorDomini.importarUsuaris(absolutePath);
     }
 
+    /**
+     * Modificadora de la contrasenya de l'usuari amb l'identificador donat
+     * @param id identificador de l'usuari
+     * @param novaContrasenya contrasenya nova
+     * @throws NoExisteixElementException no existeix l'element
+     */
     public void canviaContrasenyaUsuari(String id, char[] novaContrasenya) throws NoExisteixElementException {
         controladorDomini.canviaContrasenyaUsuari(id,novaContrasenya);
     }
 
+    /**
+     * Modificadora del nom del usuari amb l'identificador donat
+     * @param id identificadora del usuari
+     * @param nouNom nou nom de l'usuari
+     * @throws NoExisteixElementException
+     */
     public void canviaNomUsuari(String id, String nouNom) throws NoExisteixElementException {
         controladorDomini.canviaNomUsuari(id,nouNom);
     }
 
+    /**
+     * Exporta les valoracions a la localització donada
+     * @param absolutePath path on volem guardar l'arxiu
+     * @throws IOException Error en l'entrada/sortida
+     */
     public void exportaValoracions(String absolutePath) throws IOException {
         controladorDomini.exportaValoracions(absolutePath);
     }
