8b03778f7ef2efa5f2fade5101cbd5c85016d292
mariaprat <maria.prat.colomer@gmail.com>
2021-12-22 23:58:09 +0100

    L'entrega més bonica del món: 978 commits

--- FONTS/presentacio/vistes/VistaDialegAutors.java
+++ FONTS/presentacio/vistes/VistaDialegAutors.java
@@ -15,7 +15,7 @@ public class VistaDialegAutors extends JDialog {
         setResizable(false);
 
         JPanel panell = new JPanel(new BorderLayout());
-        setTitle("PROP 2020-2021 Q1 - FIB UPC");
+        setTitle("PROP 2020-2021 Q1  -  FIB UPC");
 
         JLabel text = new JLabel();
         text.setText("<html><h1>Grup 31 - Subgrup 7.1</h1> <h2>Autors:</h2> <div style='padding-left:10px'> Pol Casacuberta Gil <br> Edgar Moreno Martinez <br> Maria Prat Colomer <br> Pablo Vega Gallego <br> </div> <h2> Tutor:</h2> <div style='padding-left:10px'> Sergio Alvarez </div></html>");
